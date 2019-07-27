708f0eea7c61096531deab9637c266c894f04b4c
Miguel Torres <torresmiguel@protonmail.ch>
2019-07-27 11:40:51 -0700

    interaction Trigger, Interaction UI

---
+++ Assets/SDGJ2019/Scripts/UI/UIInteraction.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Robots
+{
+    public class UIInteraction : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject interactablePanel;
+
+        [SerializeField]
+        TextMeshProUGUI text;
+
+        bool interactionAvailable = false;
+
+        private void Awake()
+        {
+            InteractionTrigger.OnInteractionAvailable += OnInteractionAvailable;
+            InteractionTrigger.OnInteractionUnavailable += OnInteractionUnavailable;
+
+            DialogueManager.OnDialogueStarted += OnDialogueStarted;
+            DialogueManager.OnDialogueEnded += OnDialogueEnded;
+
+            interactablePanel.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            InteractionTrigger.OnInteractionAvailable -= OnInteractionAvailable;
+            InteractionTrigger.OnInteractionUnavailable -= OnInteractionUnavailable;
+
+
+            DialogueManager.OnDialogueStarted -= OnDialogueStarted;
+            DialogueManager.OnDialogueEnded -= OnDialogueEnded;
+        }
+
+        private void OnDialogueStarted()
+        {
+            interactablePanel.SetActive(false);
+        }
+
+        private void OnDialogueEnded()
+        {
+            if (interactionAvailable)
+            {
+
+                interactablePanel.SetActive(true);
+            }
+        }
+
+        private void OnInteractionAvailable(InteractionTypes interaction)
+        {
+            interactionAvailable = true;
+            interactablePanel.SetActive(true);
+
+            text.text = interaction.ToString();
+
+        }
+
+        private void OnInteractionUnavailable(InteractionTypes interaction)
+        {
+            interactionAvailable = false;
+            interactablePanel.SetActive(false);
+        }
+    }
+}
