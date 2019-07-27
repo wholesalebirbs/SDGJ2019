23f438a397efcb552f308fc57e83bdb8fab1a3b6
Garrison Price <dastyruck@gmail.com>
2019-07-27 11:28:30 -0700

    Player camera tweaks Basic enemy behaviour and movement

---
+++ Assets/SDGJ2019/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
