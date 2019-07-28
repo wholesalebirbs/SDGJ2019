833bd79efbbd2276603bf1bf979eaec97e9fe5bf
Garrison Price <garrison.b.price@gmail.com>
2019-07-28 16:16:23 -0700

    Enemies are now destroyed by lane blasts

--- Assets/SDGJ2019/Scripts/LaneBlast.cs
+++ Assets/SDGJ2019/Scripts/LaneBlast.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Walkman;
 
 public class LaneBlast : MonoBehaviour
 {
@@ -10,5 +11,13 @@ public class LaneBlast : MonoBehaviour
     void Update()
     {
         this.transform.Translate(Vector3.right * Time.deltaTime * blastSpeed);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.transform.gameObject.GetComponent<OnBeatMover>() != null)
+        {
+            GameObject.Destroy(collider.transform.gameObject);
+        }
     }
 }
