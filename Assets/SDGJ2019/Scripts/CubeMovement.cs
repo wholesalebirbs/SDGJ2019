54b95d1601e387e6a4f159c57cc97d407b9f7730
Garrison Price <garrison.b.price@gmail.com>
2019-07-28 13:05:09 -0700

    Added snappyness to cube movement

--- Assets/SDGJ2019/Scripts/CubeMovement.cs
+++ Assets/SDGJ2019/Scripts/CubeMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CubeMovement : MonoBehaviour
 {
     public float bpm = 60;
+    [Range(2,20)]
+    public float cubeSnappiness = 5;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,8 @@ public class CubeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.transform.rotation = Quaternion.Euler((float)AudioSettings.dspTime * bpm / 60 * 360 / 4, 0, 0);
-        this.transform.position = new Vector3(0, Mathf.Sqrt(1 - Mathf.Pow(((((float)AudioSettings.dspTime * bpm / 60) + 1) * 2) % 2 - 1,2)) * 0.207f + 0.5f, 0);//* Mathf.Abs((Time.time * 60 / bpm) % 1 - 0.5f) * Mathf.Sqrt(2) / 2 + 0.5f, 0);
+        float t = Mathf.Pow(((float)AudioSettings.dspTime * bpm / 60) % 1,cubeSnappiness);
+        this.transform.rotation = Quaternion.Euler(t * 360 / 4, 0, 0);
+        this.transform.position = new Vector3(0, Mathf.Sqrt(1 - Mathf.Pow(((t + 1) * 2) % 2 - 1,2)) * 0.207f + 0.5f, 0);//* Mathf.Abs((Time.time * 60 / bpm) % 1 - 0.5f) * Mathf.Sqrt(2) / 2 + 0.5f, 0);
     }
 }
