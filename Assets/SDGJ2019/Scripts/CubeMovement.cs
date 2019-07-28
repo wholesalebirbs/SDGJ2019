e92748bd3a523d0e47578e11ffc5dc5b1e22a2b0
Garrison Price <garrison.b.price@gmail.com>
2019-07-28 14:39:57 -0700

    Added cube movement on input Scrapped anything that looks pretty #cursedjam

--- Assets/SDGJ2019/Scripts/CubeMovement.cs
+++ Assets/SDGJ2019/Scripts/CubeMovement.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour
 {
-    public float bpm = 60;
-    [Range(2,20)]
-    public float cubeSnappiness = 5;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //public float bpm = 60;
+    //[Range(2,20)]
+    //public float cubeSnappiness = 5;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
 
-    // Update is called once per frame
-    void Update()
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    float currentBeat = (float)AudioSettings.dspTime * bpm / 60;
+    //    float t = Mathf.Pow(currentBeat % 1, cubeSnappiness);
+
+    //    this.transform.localRotation = Quaternion.Euler((t + (int)currentBeat) * 360 / 4, 0, 0);
+    //    this.transform.localPosition = new Vector3(0, Mathf.Sqrt(1 - Mathf.Pow(((t + 1) * 2) % 2 - 1,2)) * 0.207f + 0.5f, 0);//* Mathf.Abs((Time.time * 60 / bpm) % 1 - 0.5f) * Mathf.Sqrt(2) / 2 + 0.5f, 0);
+    //}
+
+    private int currentPosition = 3;
+
+    public void OnValidInput(string direction)
     {
-        float currentBeat = (float)AudioSettings.dspTime * bpm / 60;
-        float t = Mathf.Pow(currentBeat % 1, cubeSnappiness);
-        
-        this.transform.rotation = Quaternion.Euler((t + (int)currentBeat) * 360 / 4, 0, 0);
-        this.transform.position = new Vector3(0, Mathf.Sqrt(1 - Mathf.Pow(((t + 1) * 2) % 2 - 1,2)) * 0.207f + 0.5f, 0);//* Mathf.Abs((Time.time * 60 / bpm) % 1 - 0.5f) * Mathf.Sqrt(2) / 2 + 0.5f, 0);
+        if (direction == "Up")
+        {
+            if (currentPosition > 0)
+            {
+                this.transform.Translate(Vector3.up);
+                currentPosition--;
+            }
+        }
+        else if (direction == "Down")
+        {
+            if (currentPosition < 5)
+            {
+                this.transform.Translate(Vector3.down);
+                currentPosition++;
+            }
+        }
     }
 }
--- Assets/SDGJ2019/Scripts/OnBeatMover.cs
+++ Assets/SDGJ2019/Scripts/OnBeatMover.cs
@@ -13,6 +13,18 @@ namespace Walkman
 
         private int currentBeats;
 
+        public float bpm = 120;
+
+        private Vector3 startPosition;
+        private float startBeat;
+
+
+        public void Start()
+        {
+            startPosition = this.transform.position;
+            startBeat = (float)AudioSettings.dspTime * bpm / 60;
+        }
+
         public void OnBeat()
         {
             currentBeats++;
@@ -28,6 +40,20 @@ namespace Walkman
         {
             transform.Translate(direction);
         }
+
+        
+        //void Update()
+        //{
+        //    float currentBeat = (float)AudioSettings.dspTime * bpm / 60;
+
+        //    float t = Mathf.Pow(currentBeat % 1, 5);
+
+        //    float formulizedT = Mathf.Sqrt(1 - Mathf.Pow((((t) + 1) * 2) % 2 - 1, 2));
+
+        //    Vector3 nextPosition = startPosition + (1 + (int)(currentBeat - startBeat)) * direction;
+
+        //    this.transform.position = Vector3.Lerp(nextPosition - direction, nextPosition, formulizedT);
+        //}
     }
 
 }
